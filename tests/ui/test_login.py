af25b0ca5f2c9350fc976ceeca1ea53fce1d2bef
elisa6454 <79989274+elisa6454@users.noreply.github.com>
2026-08-15 13:59:13 +0900

    TestCase_Login_002
    Login with invalid password

--- tests/ui/test_login.py
+++ tests/ui/test_login.py
@@ -13,5 +13,24 @@ def test_login_success():
     login_page.enter_email("qatest1234@example.com")
     login_page.enter_password("1234")
     login_page.click_login_button()
+    
     assert "Logged in as" in driver.page_source # 현재 페이지 HTML에 Logged in as라는 글자가 반드시 있어야 한다. 없으면 Failed
+    
+    driver.quit()
+    
+def test_login_wrong_password():
+    driver = webdriver.Chrome()
+    driver.get("https://automationexercise.com")
+    driver.find_element(By.LINK_TEXT, "Signup / Login").click()
+    
+    login_page = LoginPage(driver) # login_page 연결
+    
+    #TC_LOGIN_002
+    login_page.enter_email("qatest1234@example.com")
+    login_page.enter_password("4321")
+    login_page.click_login_button()
+    
+    assert login_page.get_error_message() == "Your email or password is incorrect!"    
+    # 오류 문구가 같은지 체크
+    
     driver.quit()
