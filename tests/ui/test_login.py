1af9048d35ffdba61ee00474acdf5f89dd2d7334
elisa6454 <79989274+elisa6454@users.noreply.github.com>
2026-08-15 21:07:56 +0900

    Login test case
    TC_LOGIN_003 - TC_LOGIN_007

--- tests/ui/test_login.py
+++ tests/ui/test_login.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from pages.login_page import LoginPage
 
+
+
 def test_login_success():
     driver = webdriver.Chrome()
     driver.get("https://automationexercise.com")
@@ -33,4 +35,80 @@ def test_login_wrong_password():
     assert login_page.get_error_message() == "Your email or password is incorrect!"    
     # 오류 문구가 같은지 체크
     
+    driver.quit()
+    
+def test_login_without_password():
+    driver = webdriver.Chrome()
+    driver.get("https://automationexercise.com")
+    driver.find_element(By.LINK_TEXT, "Signup / Login").click()
+    
+    login_page = LoginPage(driver) # login_page 연결
+    
+    #TC_LOGIN_003
+    login_page.enter_email("qatest1234@example.com")
+    login_page.click_login_button()      
+    
+    assert login_page.get_validation_message("password")== "이 입력란을 작성하세요."  
+    
+    driver.quit()
+    
+def test_login_unregistered_email():
+    driver = webdriver.Chrome()
+    driver.get("https://automationexercise.com")
+    driver.find_element(By.LINK_TEXT, "Signup / Login").click()
+    
+    login_page = LoginPage(driver) # login_page 연결
+    
+    #TC_LOGIN_004
+    login_page.enter_email("testtest@example.com")
+    login_page.enter_password("1234")
+    login_page.click_login_button()
+    
+    assert login_page.get_error_message() == "Your email or password is incorrect!"
+
+    driver.quit()
+    
+def test_login_invalid_email_format():
+    driver = webdriver.Chrome()
+    driver.get("https://automationexercise.com")
+    driver.find_element(By.LINK_TEXT, "Signup / Login").click()
+
+    login_page = LoginPage(driver) # login_page 연결
+
+    #TC_LOGIN_005
+    login_page.enter_email("qatest.com")
+    login_page.enter_password("1234")
+    login_page.click_login_button()
+
+    assert login_page.get_validation_message("email") == "이메일 주소에 '@'를 포함해 주세요. 'qatest.com'에 '@'가 없습니다."
+
+    driver.quit()
+    
+def test_login_without_email():
+    driver = webdriver.Chrome()
+    driver.get("https://automationexercise.com")
+    driver.find_element(By.LINK_TEXT, "Signup / Login").click()
+        
+    login_page = LoginPage(driver) # login_page 연결
+        
+    #TC_LOGIN_006
+    login_page.enter_password("1234")
+    login_page.click_login_button()      
+        
+    assert login_page.get_validation_message("email") == "이 입력란을 작성하세요."  
+        
+    driver.quit()
+    
+def test_login_empty_credentials():
+    driver = webdriver.Chrome()
+    driver.get("https://automationexercise.com")
+    driver.find_element(By.LINK_TEXT, "Signup / Login").click()
+
+    login_page = LoginPage(driver) # login_page 연결
+
+    #TC_LOGIN_007
+    login_page.click_login_button()
+
+    assert login_page.get_validation_message("email") == "이 입력란을 작성하세요."
+
     driver.quit()
